bfca6cb2641e5cd321bfda8c85ca0d2978220531
Pky <1414463123@qq.com>
2018-10-12 18:24:56 +0800

    WIP

--- Pencil.py
+++ Pencil.py
@@ -1,9 +1,16 @@
 import numpy as np
 
+
 class Pencil:
 
     @staticmethod
+    def set_width_height(plt, width, height):
+        plt.xlim(0, width)
+        plt.ylim(0, height)
+
+    @staticmethod
     def stroke_line(plt, point_A=[0, 0], point_B=[0, 0], line_style='-', marker='.', color='c', alpha=1):
+        plt.axis('equal')
         plt.plot([point_A[0], point_B[0]], [point_A[1], point_B[1]], linestyle=line_style, marker=marker, color=color,
                  alpha=alpha)
 
@@ -18,22 +25,25 @@ class Pencil:
     @staticmethod
     def fill_circle(plt, O=[0, 0], R=1, color='c', alpha=1):
         plt.axis('equal')
-        theta = np.linspace(0, np.pi * 2, 500)
+        theta = np.linspace(0, np.pi * 2, 100)
         circle_point_x = O[0] + R * np.cos(theta)
         circle_point_y = O[1] + R * np.sin(theta)
         for i in range(len(theta)):
-            plt.plot([O[0], circle_point_x[i]], [O[1], circle_point_y[i]], color=color, alpha=alpha)
+            plt.plot([O[0], circle_point_x[i]], [O[1], circle_point_y[i]], color=color, alpha=alpha, linewidth=10)
 
     @staticmethod
     def stroke_rect(plt, center=[0, 0], width=1, height=1, color='c', line_style='-', alpha=1):
-        horn_x = [center[0] - width / 2, center[0] + width / 2, center[0] + width / 2, center[0] - width / 2, center[0] - width / 2]
-        horn_y = [center[1] + height / 2, center[1] + height / 2, center[1] - height / 2, center[1] - height / 2, center[1] + height / 2]
+        plt.axis('equal')
+        horn_x = [center[0] - width / 2, center[0] + width / 2, center[0] + width / 2, center[0] - width / 2,
+                  center[0] - width / 2]
+        horn_y = [center[1] + height / 2, center[1] + height / 2, center[1] - height / 2, center[1] - height / 2,
+                  center[1] + height / 2]
         plt.plot(horn_x, horn_y, color=color, linestyle=line_style, alpha=alpha)
 
     @staticmethod
     def fill_rect(plt, center=[0, 0], width=1, height=1, color='c', line_style='-', alpha=1):
-        integrate_y = np.arange(center[1] - height / 2, center[1] + height / 2, 0.001)
+        plt.axis('equal')
+        integrate_y = np.arange(center[1] - height / 2, center[1] + height / 2, 0.01)
         for i in range(len(integrate_y)):
             plt.plot([center[0] - width / 2, center[0] + width / 2], [integrate_y[i], integrate_y[i]], color=color,
-                     linestyle=line_style, alpha=alpha)
-
+                     linestyle=line_style, alpha=alpha, linewidth=10)
--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    Pencil.stroke_line(plt, [0, 0], [10, 10], line_style='--', color='r', alpha=0.5)
-    Pencil.fill_rect(plt, [1, 1], 1, 1)
+    Pencil.set_width_height(plt, 1, 1)
+    # Pencil.stroke_line(plt, [0, 0], [10, 10], line_style='--', color='r', alpha=0.5)
+    Pencil.fill_circle(plt, [0, 0], 1)
+    Pencil.fill_rect(plt, [10, 10], 10, 10)
     plt.show()
 
 
